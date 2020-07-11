1c42094e74385619e9575b803096a308ac7bd4ca
Darenn <keller.darenn@gmail.com>
2020-07-11 10:24:31 +0200

    Nme work way better now!

--- Viruses/NME.gd
+++ Viruses/NME.gd
@@ -1,33 +1,52 @@
-extends KinematicBody2D
+extends Node2D
 
-var direction = Vector2.DOWN
+var _direction = Vector2.DOWN
 var speed = 50
+
+var _available_directions = [Vector2.DOWN, Vector2.UP, Vector2.RIGHT, Vector2.LEFT]
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	var collision: KinematicCollision2D = move_and_collide(direction * speed * delta)
-	if collision:
-		var collision_direction = (collision.position - position).normalized()
-		if abs(collision.position.x) > abs(collision.position.y):
-			direction = _get_random_direction([Vector2.LEFT, Vector2.RIGHT])
-#			if collision.position.x < 0:
-#				position.x += 2
-#			if collision.position.x > 0:
-#				position.x -= 2
-		else:
-			direction = _get_random_direction([Vector2.UP, Vector2.DOWN])
-#			if collision.position.y > 0:			
-#				position.y -= 2
-#			if collision.position.y < 0:
-#				position.y += 2
-	
+	# If we detect a wall in our current direction, change direction
+	if not _available_directions.has(_direction):
+		var favorite_directions = _available_directions.duplicate()
+		# Avoid turn down
+		favorite_directions.erase(-_direction)
+		if favorite_directions.size() == 0:
+			favorite_directions = _available_directions.duplicate()
+		_direction = favorite_directions[randi() % favorite_directions.size()]
+	position += _direction * speed * delta
 
-func _get_random_direction(directions_to_avoid:= [Vector2.ZERO]):
-	var directions = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
-	var favorite_directions = []
-	for direction in directions:
-		if not directions_to_avoid.has(direction):
-			favorite_directions.append(direction)
-	return directions[randi() % directions.size()]
+
+func _on_AreaRight_body_entered(body: Node) -> void:
+	_available_directions.erase(Vector2.RIGHT)
+
+
+func _on_AreaRight_body_exited(body: Node) -> void:
+	_available_directions.append(Vector2.RIGHT)
+
+
+func _on_AreaLeft_body_entered(body: Node) -> void:
+	_available_directions.erase(Vector2.LEFT)
+
+
+func _on_AreaLeft_body_exited(body: Node) -> void:
+	_available_directions.append(Vector2.LEFT)
+
+
+func _on_AreaUp_body_entered(body: Node) -> void:
+	_available_directions.erase(Vector2.UP)
+
+
+func _on_AreaUp_body_exited(body: Node) -> void:
+	_available_directions.append(Vector2.UP)
+
+
+func _on_AreaDown_body_entered(body: Node) -> void:
+	_available_directions.erase(Vector2.DOWN)
+
+
+func _on_AreaDown_body_exited(body: Node) -> void:
+	_available_directions.append(Vector2.DOWN)
